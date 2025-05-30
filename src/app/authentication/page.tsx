2edda12991481d233dcf42166cb3df2aa54a505d
Houston Braga <houstonliverboy123@gmail.com>
2025-05-29 21:27:56 -0300

    feat: authentication in the authentication page

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,17 +1,30 @@
-"use client";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { auth } from "@/lib/auth";
 
 import LoginForm from "./components/login-form";
 import SignUpForm from "./components/sign-up-form";
 
-const AuthenticationPage = () => {
+const AuthenticationPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session?.user) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-slate-300">
       <Tabs defaultValue="login" className="w-[400px]">
-        <TabsList className="bg-emerald-200 grid w-full grid-cols-2">
-          <TabsTrigger value="login" className="cursor-pointer">Login</TabsTrigger>
-          <TabsTrigger value="register" className="cursor-pointer">Cadastre-se</TabsTrigger>
+        <TabsList className="grid w-full grid-cols-2 bg-emerald-200">
+          <TabsTrigger value="login" className="cursor-pointer">
+            Login
+          </TabsTrigger>
+          <TabsTrigger value="register" className="cursor-pointer">
+            Cadastre-se
+          </TabsTrigger>
         </TabsList>
         <TabsContent value="login">
           <LoginForm />
