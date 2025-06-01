7a4ff64f7ebb6791e4ab9d8baf9b8098dc03a7f3
Houston Braga <houstonliverboy123@gmail.com>
2025-06-01 13:30:58 -0300

    fix: add optional chainning on plugin customSession

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -33,8 +33,8 @@ export const auth = betterAuth({
         user: {
           ...user,
           clinic: {
-            id: clinic.clinicId,
-            name: clinic.clinic.name,
+            id: clinic?.clinicId,
+            name: clinic?.clinic.name,
           },
         },
         session,
