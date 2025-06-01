d33d83f3097bfca5dda4b687d5659579bb93da78
Houston Braga <houstonliverboy123@gmail.com>
2025-06-01 16:05:08 -0300

    feat: add composition pattern page-container in doctorsPage

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -1,9 +1,37 @@
+import { PlusIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  PageActions,
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/ui/page-container";
+//elaborado com a estrutura de composition pattern
+
 const DoctorsPage = () => {
   return (
-    <div>
-      <h1>Doctors</h1>
-    </div>
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Médicos</PageTitle>
+          <PageDescription>Gerencie seus médicos cadastrados</PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <Button>
+            <PlusIcon className="h-4 w-4" />
+            Adicionar Médico
+          </Button>
+        </PageActions>
+      </PageHeader>
+      <PageContent>
+        <h1>medicos</h1>
+      </PageContent>
+    </PageContainer>
   );
-}
+};
 
-export default DoctorsPage
+export default DoctorsPage;
