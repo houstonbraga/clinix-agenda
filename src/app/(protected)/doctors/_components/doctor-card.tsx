79d0a440cbf8ba8c10e7fd6c7336833c86dba879
Houston Braga <houstonliverboy123@gmail.com>
2025-06-02 00:42:08 -0300

    fix: edit list doctors

---
+++ src/app/(protected)/doctors/_components/doctor-card.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { CalendarIcon, ClockIcon, DollarSignIcon } from "lucide-react";
+
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { Separator } from "@/components/ui/separator";
+import { doctorsTable } from "@/db/schema";
+import { formatCurrencyInCents } from "@/helpers/currency";
+
+import { getAvailability } from "../_helpers/availability";
+
+interface DoctorCardProps {
+  doctor: typeof doctorsTable.$inferSelect;
+}
+
+const DoctorCard = ({ doctor }: DoctorCardProps) => {
+  const doctorInitials = doctor.name
+    .split(" ")
+    .map((name) => name[0])
+    .join("");
+
+  const availability = getAvailability(doctor);
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex items-center gap-2">
+          <Avatar className="h-14 w-14">
+            <AvatarFallback>{doctorInitials}</AvatarFallback>
+          </Avatar>
+          <div>
+            <h3 className="font-medium">{doctor.name}</h3>
+            <p className="text-muted-foreground text-sm">{doctor.specialty}</p>
+          </div>
+        </div>
+      </CardHeader>
+      <Separator />
+      <CardContent className="flex flex-col gap-1">
+        <Badge variant="outline" className="rounded-full">
+          <CalendarIcon />
+          {availability.from.format("dddd")} a {availability.to.format("dddd")}
+        </Badge>
+
+        <Badge variant="outline" className="rounded-full">
+          <ClockIcon />
+          {availability.from.format("HH:mm")} -{" "}
+          {availability.to.format("HH:mm")}
+        </Badge>
+
+        <Badge variant="outline" className="rounded-full">
+          <DollarSignIcon />
+          {formatCurrencyInCents(doctor.appointmentPriceInCents)}
+        </Badge>
+      </CardContent>
+      <Separator />
+      <CardFooter>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="w-full">Ver detalhes</Button>
+          </DialogTrigger>
+        </Dialog>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default DoctorCard;
