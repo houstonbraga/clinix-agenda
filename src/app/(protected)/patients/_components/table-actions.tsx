fef467d1fe80470f433d5b4eb82d4f42be44bd0e
Houston Braga <houstonliverboy123@gmail.com>
2025-06-06 01:41:33 -0300

    feat: ensure patients can be deleted

--- src/app/(protected)/patients/_components/table-actions.tsx
+++ src/app/(protected)/patients/_components/table-actions.tsx
@@ -1,6 +1,19 @@
 import { EditIcon, MoreHorizontalIcon, TrashIcon } from "lucide-react";
+import { useAction } from "next-safe-action/hooks";
 import { useState } from "react";
+import { toast } from "sonner";
 
+import { deletePatient } from "@/actions/delete-patient";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Dialog } from "@/components/ui/dialog";
 import {
   DropdownMenu,
@@ -20,6 +33,21 @@ interface UpsertPatientFormProps {
 
 const PatientTableActions = ({ patient }: UpsertPatientFormProps) => {
   const [upsertDialogIsOpen, setUpsertDialogIsOpen] = useState(false);
+  const deletePatientAction = useAction(deletePatient, {
+    onSuccess: () => {
+      toast.success("Paciente excluido com sucesso!");
+    },
+
+    onError: () => {
+      toast.error("Erro ao excluir paciente!");
+    },
+  });
+
+  const handleDeletePatient = () => {
+    if (!patient) return;
+    deletePatientAction.execute({ id: patient.id });
+  };
+
   return (
     <Dialog open={upsertDialogIsOpen} onOpenChange={setUpsertDialogIsOpen}>
       <DropdownMenu>
@@ -29,18 +57,37 @@ const PatientTableActions = ({ patient }: UpsertPatientFormProps) => {
         <DropdownMenuContent>
           <DropdownMenuLabel>{patient.name}</DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <DropdownMenuItem 
-            onClick={() => setUpsertDialogIsOpen(true)}>
+          <DropdownMenuItem onClick={() => setUpsertDialogIsOpen(true)}>
             <EditIcon size={16} className="mr-2" />
             Editar
           </DropdownMenuItem>
-          <DropdownMenuItem>
-            <TrashIcon size={16} className="mr-2" />
-            Excluir
-          </DropdownMenuItem>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+                <TrashIcon size={16} className="mr-2" />
+                Excluir
+              </DropdownMenuItem>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                Tem certeza que deseja excluir esse paciente?{" "}
+              </AlertDialogHeader>
+              <AlertDialogDescription>
+                Essa ação irá excluir permanentemente o paciente.
+              </AlertDialogDescription>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                <AlertDialogAction onClick={handleDeletePatient}>Excluir</AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
         </DropdownMenuContent>
       </DropdownMenu>
-      <UpsertPatientForm patient={patient} isOpen={upsertDialogIsOpen} onSuccess={() => setUpsertDialogIsOpen(false)}/>
+      <UpsertPatientForm
+        patient={patient}
+        isOpen={upsertDialogIsOpen}
+        onSuccess={() => setUpsertDialogIsOpen(false)}
+      />
     </Dialog>
   );
 };
